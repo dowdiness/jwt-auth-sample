436d546a445548cf8483e5d4942b352c61276890
Koji Ishimoto <koji.ishimoto@gmail.com>
2019-12-25 20:49:41 +0900

    Web: Set up apollo client

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
 
 const App: React.FC = () => {
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `)
+
+  if(loading) return <div>Loading...</div>
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello</h1>
+        <h1>{JSON.stringify(data)}</h1>
       </header>
     </div>
   );
