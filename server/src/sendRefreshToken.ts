eeb686eaefea2b3bdc3a707fe7667776b1e2efa3
Koji Ishimoto <koji.ishimoto@gmail.com>
2019-12-26 18:36:02 +0900

    Disable path: '/refresh_token' in sendRefreshToken

--- server/src/sendRefreshToken.ts
+++ server/src/sendRefreshToken.ts
@@ -1,7 +1,8 @@
 import { Response } from 'express'
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie('jid', token, {
-    httpOnly: true,
-    path: '/refresh_token'
+    httpOnly: true
+    // domain: '.a-yo.jp'
+    // path: '/refresh_token'
   })
 }
